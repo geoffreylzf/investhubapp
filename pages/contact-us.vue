8c4fe7a9c15f8fdbdf9ec3a698e97602446563d6
Geoffrey <geoffreylzf@gmail.com>
2021-12-20 15:49:21 +0800

    20211220 b

--- pages/contact-us.vue
+++ pages/contact-us.vue
@@ -2,6 +2,16 @@
   <UtilAdsContainer>
     <div class="m-16">
       <h1>Contact Us</h1>
+      If you have any <br />
+      <ul>
+        <li>Question</li>
+        <li>Enquiry</li>
+        <li>Opinion</li>
+        <li>Suggestion</li>
+        <li>Feedback</li>
+        <li>Complain</li>
+      </ul>
+      Please let us know by send to
       <a href="mailto:investhub2022@gmail.com">investhub2022@gmail.com</a>
     </div>
   </UtilAdsContainer>
